c2d690a59a19cfdd37871d3ebdb2d23e36182c88
Mariel Couto <coutomariel@gmail.com>
2021-07-07 15:34:18 -0300

    Personalização do status code e header location no endpoint de criação de novos autores

--- src/main/kotlin/br/com/zupacademy/autores/CadastraAutorController.kt
+++ src/main/kotlin/br/com/zupacademy/autores/CadastraAutorController.kt
@@ -1,9 +1,11 @@
 package br.com.zupacademy.autores
 
 import br.com.zupacademy.domain.repositories.AutoresRepository
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.validation.Validated
 import javax.validation.Valid
 
@@ -12,9 +14,10 @@ import javax.validation.Valid
 class CadastraAutorController (val autoresRepository: AutoresRepository){
 
     @Post
-    fun cadastra(@Body @Valid request: NovoAutorRequest){
+    fun cadastra(@Body @Valid request: NovoAutorRequest) : HttpResponse<Any>{
         val autor = request.paraAutor()
-        println(autor)
-        autoresRepository.save(autor)
+        val autorSalvo = autoresRepository.save(autor)
+        val uri = UriBuilder.of("/autores/{id}").expand(mutableMapOf(Pair("id", autorSalvo.id)))
+        return HttpResponse.created(uri)
     }
 }
