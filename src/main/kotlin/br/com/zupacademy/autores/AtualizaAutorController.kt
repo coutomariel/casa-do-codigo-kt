04749bcafb626bf34caa3a074f61312ff00350c5
Mariel Couto <coutomariel@gmail.com>
2021-07-08 08:59:26 -0300

    Implementação de endpoint para atualização da descrição de autor

---
+++ src/main/kotlin/br/com/zupacademy/autores/AtualizaAutorController.kt
@@ -0,0 +1,26 @@
+package br.com.zupacademy.autores
+
+import br.com.zupacademy.domain.repositories.AutoresRepository
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.PathVariable
+import io.micronaut.http.annotation.Put
+import io.micronaut.validation.Validated
+
+@Validated
+@Controller("/autores/{id}")
+class AtualizaAutorController(val autoresRepository: AutoresRepository) {
+
+    @Put
+    fun atualiza(@PathVariable id: Long, descricao: String): HttpResponse<Any> {
+        if (!autoresRepository.existsById(id)) {
+            return HttpResponse.notFound()
+        }
+
+        val autor = autoresRepository.findById(id).get()
+        autor.descricao = descricao
+        autoresRepository.update(autor)
+        return HttpResponse.ok("Descrição alterada")
+
+    }
+}
